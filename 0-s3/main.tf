88bef9ba4166e82f5e38fdda0f18e98da5f8ad64
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-14 21:53:55 +0530

    Update main.tf

--- 0-s3/main.tf
+++ 0-s3/main.tf
@@ -1,6 +1,7 @@
 # S3-MODULE Calling
 module "s3" {
   source = "git::https://github.com/vaheedgit26/Infra//modules/s3"    # Give the path to S3 MODULE accordingly
-  env = "dev"
-  region = "ap-south-1"
+  s3_bucket_name = "tfstate-${var.project_name}-${var.env}-${var.region}"
+  env = var.env
+  region = var.region
 }
