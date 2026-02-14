e4694bb54d8530c14d9a2763b1230e8aa605b024
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-14 21:34:02 +0530

    Update main.tf

--- 0-s3/main.tf
+++ 0-s3/main.tf
@@ -1,5 +1,5 @@
 # S3-MODULE Calling
-module "vpc" {
+module "s3" {
   source = "git::https://github.com/vaheedgit26/Infra//modules/s3"    # Give the path to S3 MODULE accordingly
   env = "dev"
   region = "ap-south-1"
