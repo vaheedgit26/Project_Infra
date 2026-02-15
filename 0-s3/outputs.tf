efde732e4db95ecbbade3c8ee4784c2edaf51122
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-15 20:03:27 +0530

    Update outputs.tf

--- 0-s3/outputs.tf
+++ 0-s3/outputs.tf
@@ -1,3 +1,8 @@
+output "aws_region" {
+  description = "AWS region"
+  value       = var.region
+}
+
 output "tfstate_bucket_arn" {
   description = "ARN of the Terraform remote state S3 bucket"
   value       = module.s3.tfstate_bucket_arn
