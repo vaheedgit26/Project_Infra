4ba93b7c1c8a54307b9c9ea2872cebc764d4674e
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-15 21:12:47 +0530

    Update create-infra.sh

--- 0-s3/create-infra.sh
+++ 0-s3/create-infra.sh
@@ -11,9 +11,9 @@ echo "============================================="
 
 terraform init
 
-echo "================================================="
-echo "Step 2: Checking S3 bucket Configuration Valid ? "
-echo "================================================="
+echo "===================================================="
+echo "Step 2: Checking S3 bucket Configuration Validity ? "
+echo "===================================================="
 terraform validate
 
 echo "================================================"
@@ -21,11 +21,14 @@ echo "Step 3: Generating plan for creating S3 bucket  "
 echo "================================================"
 terraform plan
 
+echo "================================================"
+echo "Step 4: Applying plan for creating S3 bucket  "
+echo "================================================"
 terraform apply # -auto-approve
 
 # Export S3 bucket name and AWS region as environment variables
-export TF_STATE_BUCKET=$(terraform output -raw tfstate_bucket_id)
-export AWS_REGION=$(terraform output -raw aws_region)
+export TF_VAR_tf_state_bucket=$(terraform output -raw tfstate_bucket_id)
+export TF_VAR_region=$(terraform output -raw region)
 
-echo "TF_STATE_BUCKET=$TF_STATE_BUCKET"
-echo "AWS_REGION=$AWS_REGION"
+echo "TF_VAR_tf_state_bucket=$TF_VAR_tf_state_bucket"
+echo "TF_VAR_region=$TF_VAR_region"
