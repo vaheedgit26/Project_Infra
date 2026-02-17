17136908dea6ce08da9031e39861499f8837f479
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-17 10:48:06 +0530

    Update main.tf

--- 0-s3/main.tf
+++ 0-s3/main.tf
@@ -2,8 +2,8 @@
 module "s3" {
   source = "git::https://github.com/vaheedgit26/Infra//modules/s3" # Give the path to S3 MODULE accordingly
 
-  s3_bucket_name = "tfstate-${var.project_name}-${var.region}"
+  s3_bucket_name = "tfstate-${var.project_name}-${var.env}-${var.region}"
   project_name   = var.project_name
-  #env            = var.env
+  env            = var.env
   region         = var.region
 }
