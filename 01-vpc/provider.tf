95b1a2064ace9bb0b16316f406b862db57fdcebf
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-17 11:47:51 +0530

    Update provider.tf

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -8,7 +8,13 @@ terraform {
   }
 
 # Remote Backend
-  backend "s3" {}   
+  backend "s3" {
+    bucket         = "tfstate-dev-us-east-1-jpjtof"
+    key            = "vpc/dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }      
 }
 
 provider "aws" {
