9678fc6ae2224c4d8dbfea39a3f4dfa604646320
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-18 08:42:23 +0530

    Update variables.tf

--- 40-security-groups/variables.tf
+++ 40-security-groups/variables.tf
@@ -1,6 +1,7 @@
 #############################   Common Variables   #########################################
 variable "project_name" { default = "expense" }
 variable "env" { default = "dev" }
+variable "remote_bucket" {}
 
 variable "common_tags" {
   default = {
