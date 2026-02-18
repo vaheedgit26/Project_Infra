3675454a6545e113ae6cf4a2ab87e24b69f34ace
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-18 18:54:57 +0530

    Update sample.tf

--- 40-security-groups/sample.tf
+++ 40-security-groups/sample.tf
@@ -1,4 +1,4 @@
-module "web_alb_sg" {
+module "sample_sg" {
   source         = "git::https://github.com/vaheedgit26/Infra.git//modules/sg"
   vpc_id         = local.vpc_id
   sg_name        = "web_alb_sg"
