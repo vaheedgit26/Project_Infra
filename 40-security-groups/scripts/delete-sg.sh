b9203c33f402d330bee55d4b2d208fb07b68bef6
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-18 19:29:32 +0530

    Update delete-sg.sh

--- 40-security-groups/scripts/delete-sg.sh
+++ 40-security-groups/scripts/delete-sg.sh
@@ -5,17 +5,19 @@
 #!/bin/bash
 set -e
 
-PROJECT_NAME=$1
-ENV=$2
-REGION=$3
+BUCKET=$(terraform -chdir=../../0-s3 output -raw bucket_id)              #${TF_VAR_tf_state_bucket}
+ENV=$(terraform -chdir=../../0-s3 output -raw env)                       #${TF_VAR_env}
+REGION=$(terraform -chdir=../../0-s3 output -raw region)                 #${TF_VAR_region}
+PROJECT_NAME=$(terraform -chdir=../../0-s3 output -raw project_name)
 
 # terraform init
 
 cd ..
 
 terraform destroy \
+  -var="bucket=${BUCKET}" \
   -var="project_name=$PROJECT_NAME" \
-  -var="env=$ENV"
+  -var="env=$ENV" \
   -var="region=$REGION"
   # -auto-approve
 
