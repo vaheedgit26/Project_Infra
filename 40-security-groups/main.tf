92ddb1684119edd745d2628a36bb3b04d920216f
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-18 07:39:03 +0530

    Update main.tf

--- 40-security-groups/main.tf
+++ 40-security-groups/main.tf
@@ -1,1 +1,11 @@
+module "web_alb_sg" {
+  source         = "git::https://github.com/vaheedgit26/Infra.git//modules/sg"
+  vpc_id         = local.vpc_id
+  sg_name        = "web_alb_sg"
+  sg_description = "Web ALB Security Group"
+
+  project_name = var.project_name
+  env          = var.env
+  common_tags  = var.common_tags
+}
 
