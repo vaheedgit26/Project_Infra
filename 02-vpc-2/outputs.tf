917fcf6c568ff6e3816066167415234008a300aa
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-21 15:23:12 +0530

    Update outputs.tf

--- 02-vpc-2/outputs.tf
+++ 02-vpc-2/outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-  value       = module.vpc.vpc_id
+  value       = module.vpc-2.vpc_id
   description = "VPC ID used by EKS and other services"
 }
 
 output "public_subnet_ids" {
-  value       = module.vpc.private_subnet_ids
+  value       = module.vpc-2.private_subnet_ids
   description = "Private subnets for EKS worker nodes"
 }
 
 output "private_subnet_ids" {
-  value       = module.vpc.public_subnet_ids
+  value       = module.vpc-2.public_subnet_ids
   description = "Public subnets for ALB, NLB, etc."
 }
 
 output "database_subnet_ids" {
-  value       = module.vpc.database_subnet_ids
+  value       = module.vpc-2.database_subnet_ids
   description = "Public subnets for ALB, NLB, etc."
 }
