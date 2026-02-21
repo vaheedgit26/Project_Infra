5661bb18937248b120cc9d222edc48edd119fb13
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-21 12:07:35 +0530

    Update data-remote-state.tf

--- 03-vpc-peering/data-remote-state.tf
+++ 03-vpc-peering/data-remote-state.tf
@@ -5,8 +5,21 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket = var.remote_bucket                 # Name of the remote S3 bucket where the VPC state is stored
-    key    = "dev/vpc/terraform.tfstate"       # Path to the VPC tfstate file within the bucket
-    region = var.region                        # Region where the S3 bucket and DynamoDB table exist
+    bucket = var.remote_bucket                        # Name of the remote S3 bucket where the VPC state is stored
+    key    = "${var.env}/vpc/terraform.tfstate"       # Path to the VPC tfstate file within the bucket
+    region = var.region                               # Region where the S3 bucket and DynamoDB table exist
+  }
+}
+
+# --------------------------------------------------------------------
+# Reference the Remote State from VPC Project
+# --------------------------------------------------------------------
+data "terraform_remote_state" "vpc-2" {
+  backend = "s3"
+
+  config = {
+    bucket = var.remote_bucket                          # Name of the remote S3 bucket where the VPC state is stored
+    key    = "${var.env}/vpc-2/terraform.tfstate"       # Path to the VPC tfstate file within the bucket
+    region = var.region                                 # Region where the S3 bucket and DynamoDB table exist
   }
 }
