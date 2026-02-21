1567715fa85976f26f596dcb351b572b50177183
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-21 11:48:50 +0530

    Create delete-vpc.sh

---
+++ 02-vpc-2/scripts/delete-vpc.sh
@@ -0,0 +1,38 @@
+########################################################
+# Usage: bash delete-vpc.sh      #
+########################################################
+
+#!/bin/bash
+set -e
+
+BUCKET=$(terraform -chdir=../../0-s3 output -raw bucket_id)              
+ENV=$(terraform -chdir=../../0-s3 output -raw env)                       
+REGION=$(terraform -chdir=../../0-s3 output -raw region)                 
+PROJECT_NAME=$(terraform -chdir=../../0-s3 output -raw project_name)
+
+echo """
+📄 Details:
+     PROJECT_NAME : ${PROJECT_NAME}
+     ENV          : ${ENV}
+     REGION       : ${REGION}
+     BUCKET       : ${BUCKET}
+"""
+
+# terraform init
+
+cd ..
+
+terraform destroy \
+  -var="project_name=$PROJECT_NAME" \
+  -var="env=$ENV" \
+  -var="region=$REGION"
+  # -auto-approve
+
+
+# terraform plan -destroy \
+#  -var="project_name=expense" \
+#  -var="env=dev" \
+#  -var="region=ap-south-1" \
+#  -out=destroy.tfplan
+
+# terraform apply destroy.tfplan
