12ba3f5d92ae60dd60bc64b313471a7b19b28fe3
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-21 10:58:17 +0530

    Update main.tf

--- 03-vpc-peering/main.tf
+++ 03-vpc-peering/main.tf
@@ -1,1 +1,7 @@
+module "vpc-peering" {
+  source = "git::https://github.com/vaheedgit26/Infra//modules/vpc-peering"   # Give the path to VPC MODULE accordingly
 
+  region                       =  var.region
+  requester_vpc_id             =  local.requester_vpc_id                       
+  accepter_vpc_id              =  local.accepter_vpc_id                        
+}
