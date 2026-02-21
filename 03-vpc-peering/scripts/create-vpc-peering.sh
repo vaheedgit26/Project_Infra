8f86994a2747b369e80a03e78360ba82709beafb
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-21 15:56:36 +0530

    Update create-vpc-peering.sh

--- 03-vpc-peering/scripts/create-vpc-peering.sh
+++ 03-vpc-peering/scripts/create-vpc-peering.sh
@@ -46,6 +46,7 @@ echo "================================================="
 # terraform plan
 terraform plan \
   -out=vpc-peering.tfplan \
+  -var="remote_bucket=${BUCKET}" \
   -var="project_name=$PROJECT_NAME" \
   -var="env=$ENV"
   -var="requester_vpc_region=$REGION" \
