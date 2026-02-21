f53a491f9e9fe2cb4f723968f2939813aa021071
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-21 12:06:56 +0530

    Update data-remote-state.tf

--- 40-security-groups/data-remote-state.tf
+++ 40-security-groups/data-remote-state.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "vpc" {
 
   config = {
     bucket = var.remote_bucket                 # Name of the remote S3 bucket where the VPC state is stored
-    key    = "dev/vpc/terraform.tfstate"       # Path to the VPC tfstate file within the bucket
+    key    = "${var.env}/vpc/terraform.tfstate"       # Path to the VPC tfstate file within the bucket
     region = var.region                        # Region where the S3 bucket and DynamoDB table exist
   }
 }
